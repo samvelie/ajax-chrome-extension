f44e037d42c728290d032b33afb0bbc9878bfe04
samvelie <sam.velie@gmail.com>
2017-03-18 14:25:39 -0500

    functioning get and post to external server

--- client.js
+++ client.js
@@ -1,10 +1,33 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngSanitize']);
 
-myApp.controller('AppController', [function() {
+
+myApp.controller('AppController', ['$http', function($http) {
   console.log('app controller running');
   var self = this;
 
   self.message = "Welcome to your new Chrome Extension!";
 
+  self.data = '';
 
+  self.newData = '';
+
+  self.requestData = function(){
+    $http({
+      method: 'GET',
+      url: 'http://localhost:5555/'
+    }).then(function(response){
+      console.log(response.data);
+      self.data = response.data;
+    });
+  };
+
+  self.sendData = function(){
+    $http({
+      method: 'POST',
+      url: 'http://localhost:5555/'
+    }).then(function(response){
+      console.log(response.data);
+
+    });
+  };
 }]);
